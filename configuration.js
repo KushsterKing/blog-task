ae362f327e68be844a6ef0a37d249d71d85b120b
kush <kushgumber@gmail.com>
2021-09-27 17:52:37 +0530

    h

--- configuration.js
+++ configuration.js
@@ -1,6 +1,7 @@
 module.exports = {
     secret: 'Fjpp5894DWEDSKA9787Lr8xu9kSDJOJhgj9kAj3AS',
-    mongoDBUrl: 'mongodb+srv://test:test@cluster0.shug4.mongodb.net/blogs?retryWrites=true&w=majority',
+    // mongoDBUrl: 'mongodb+srv://test:test@cluster0.shug4.mongodb.net/blogs?retryWrites=true&w=majority',
+    mongoDBUrl: 'mongodb+srv://kush:t7pyMYe4c4HFBwaV@realmcluster.n1ru8.mongodb.net/blogs?retryWrites=true&w=majority',
 
     // mongoDBUrl: 'mongodb://localhost/blogs',
     adminPasswordSalt: 10
