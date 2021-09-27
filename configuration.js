8c1c0ae15a045f87e832a6939ec78405d40be6db
kush <kushgumber@gmail.com>
2021-09-27 19:33:43 +0530

    done

--- configuration.js
+++ configuration.js
@@ -1,7 +1,6 @@
 module.exports = {
     secret: 'Fjpp5894DWEDSKA9787Lr8xu9kSDJOJhgj9kAj3AS',
     // mongoDBUrl: 'mongodb+srv://test:test@cluster0.shug4.mongodb.net/blogs?retryWrites=true&w=majority',
-    // mongoDBUrl: 'mongodb+srv://kush:t7pyMYe4c4HFBwaV@realmcluster.n1ru8.mongodb.net/blogs?retryWrites=true&w=majority',
 
     mongoDBUrl: 'mongodb://localhost/blogs',
     adminPasswordSalt: 10
