5c0edbd19c5e7268b54acc3382bdbe75b4f0c0ae
kush <kushgumber@gmail.com>
2021-09-27 20:11:38 +0530

    puhs

--- app/seeds/seed_admins.js
+++ app/seeds/seed_admins.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 (async () => {
 
-    let colorList = [{role: 'ADMIN', email: 'adminone@gmail.com', first_name: 'admin', last_name: 'one', password: '$2a$10$q3G9jaFBL4DPkiDcCUJvpeoDHpE0ZwggUorHK009i8f5K1wum44Mu'/*admin_one@123*/}, {role: 'ADMIN', email: 'admintwo@gmail.com', first_name: 'admin', last_name: 'two', password: '$2a$10$B3VWx0xVKMuwEfHV5nahMu.lheIRRICENEDpLqZ7x9XJTDdLbH0yG' /*admin_two@123*/}];
+    let colorList = [{role: 'ADMIN', is_deleted:false, email: 'adminone@gmail.com', first_name: 'admin', last_name: 'one', password: '$2a$10$q3G9jaFBL4DPkiDcCUJvpeoDHpE0ZwggUorHK009i8f5K1wum44Mu'/*admin_one@123*/}, {role: 'ADMIN', email: 'admintwo@gmail.com', is_deleted:false, first_name: 'admin', last_name: 'two', password: '$2a$10$B3VWx0xVKMuwEfHV5nahMu.lheIRRICENEDpLqZ7x9XJTDdLbH0yG' /*admin_two@123*/}];
 
     setTimeout(async () => {
 
